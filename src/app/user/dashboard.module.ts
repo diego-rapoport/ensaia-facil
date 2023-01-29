723b9110df4413a1d4debbc9726dd1e7c2b8744a
Diego Rapoport <diegomrapoport@gmail.com>
2023-01-29 11:11:32 -0300

    Install angular-calendar

--- src/app/user/dashboard.module.ts
+++ src/app/user/dashboard.module.ts
@@ -4,10 +4,19 @@ import { CommonModule } from '@angular/common'
 import { DashboardRoutingModule } from './dashboard-routing.module'
 import { DashboardComponent } from './dashboard.component'
 import { GoogleCalendarService } from '../shared/services/google-calendar.service'
+import { CalendarModule, DateAdapter } from 'angular-calendar'
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns'
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
   providers: [GoogleCalendarService],
 })
 export class DashboardModule {}
